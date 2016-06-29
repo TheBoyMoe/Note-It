feeca87e458e139b4b8dbc47cab966456c6a9661
billfero <billfero@gmail.com>
2016-06-29 15:05:17 +0100

    saving notes to database

--- app/src/main/java/com/example/demoapp/ui/activity/TextNoteActivity.java
+++ app/src/main/java/com/example/demoapp/ui/activity/TextNoteActivity.java
@@ -3,11 +3,14 @@ package com.example.demoapp.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.demoapp.R;
 import com.example.demoapp.common.Constants;
+import com.example.demoapp.common.Utils;
+import com.example.demoapp.model.DatabaseHelper;
 import com.example.demoapp.ui.fragment.TextNoteFragment;
 
 import timber.log.Timber;
@@ -23,8 +26,9 @@ public class TextNoteActivity extends AppCompatActivity
 
     @Override
     public void saveTextNote(String title, String description) {
-        // TODO save note to database
+        // save note to database
         Timber.i("%s, title: %s, description: %s", Constants.LOG_TAG, title, description);
+        new InsertItemThread(Utils.generateCustomId(), title, description).start();
         finish();
     }
 
@@ -41,5 +45,32 @@ public class TextNoteActivity extends AppCompatActivity
         }
     }
 
+
+    // insert item into database via a bkgd thread
+    class InsertItemThread extends Thread {
+        private long mItemId;
+        private String mTitle;
+        private String mDescription;
+
+        public InsertItemThread(long itemId, String title, String description) {
+            super();
+            mItemId = itemId;
+            mTitle = title;
+            mDescription = description;
+        }
+
+        @Override
+        public void run() {
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            try {
+                DatabaseHelper.getInstance(TextNoteActivity.this).
+                        insertTaskItem(TextNoteActivity.this, mItemId, mTitle, mDescription);
+            } catch (Exception e) {
+                Timber.e("%s: error adding item to dbase, %s", Constants.LOG_TAG, e.getMessage());
+            }
+            // query the dbase so as to trigger an update of the ui
+            Utils.queryAllItems(TextNoteActivity.this);
+        }
+    }
 
 }
